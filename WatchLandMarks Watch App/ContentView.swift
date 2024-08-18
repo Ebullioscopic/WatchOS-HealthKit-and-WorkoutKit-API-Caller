d865395432d07ba1a904d57e0309e6828b25e7fd
Ebullioscopic <hrhn.mudaliar251@gmail.com>
2024-08-18 13:41:53 +0530

    Initial Commit

---
+++ WatchLandMarks Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchLandMarks Watch App
+//
+//  Created by hariharan on 18/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchLandMarks Watch App/WatchLandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandMarksApp.swift
+//  WatchLandMarks Watch App
+//
+//  Created by hariharan on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandMarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
