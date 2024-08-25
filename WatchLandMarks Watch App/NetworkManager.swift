3ebf151c862b312a8c55f78e6140b231a4018436
Ebullioscopic <hrhn.mudaliar251@gmail.com>
2024-08-25 22:24:37 +0530

    Initial Commit

---
+++ WatchLandMarks Watch App/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  WatchLandMarks Watch App
+//
+//  Created by hariharan on 25/08/24.
+//
+
+import Foundation
---
+++ WatchLandMarks Watch App/QRCodeView.swift
@@ -0,0 +1,18 @@
+//
+//  QRCodeView.swift
+//  WatchLandMarks Watch App
+//
+//  Created by hariharan on 25/08/24.
+//
+
+import SwiftUI
+
+struct QRCodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QRCodeView()
+}
